02c597cf7d08e487d7c8f198bc9474aa29c60267
ShouryaGoje <shouryagoje@gmail.com>
2024-01-26 16:15:29 +0530

    Video 4 End, Resndering components in react

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,12 +1,15 @@
 import React, {Component} from "react";
-import {render} from "react-dom"
+import {render} from "react-dom";
+import HomePage from "./HomePage";
+
+
 
 export default class App extends Component{
     constructor(props){
         super(props);
     }
     render(){
-        return <h1>Testing React Codes</h1>;
+        return <HomePage/>;
     }
 }
 
